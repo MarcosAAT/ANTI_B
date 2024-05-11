2c88e1f126959cf11dd93b8206943f21f03ec0e7
Marcos <marcosat2001@gmail.com>
2024-05-11 17:59:10 -0400

    Added Score
    Added the score functionality.

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public Player player; 
+   // public Player player; 
+    public TextMeshProUGUI scoreText;
     private int score; 
 
     public void DecreaseScore(){
         score--; 
+        scoreText.text =  score.ToString();
     }
 
     public void IncreseScore(){
         score++; 
+        scoreText.text =  score.ToString();
     }
 
     public void Pause(){
