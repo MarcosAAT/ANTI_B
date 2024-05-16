a1f960735c3297f27ee538c4bcbb9bafa777228e
Npelletier91 <nicolas.pelletier@yahoo.com>
2024-05-16 10:47:17 -0400

    created a pixel art play button

--- My project/Assets/Scripts/MainMenu.cs
+++ My project/Assets/Scripts/MainMenu.cs
@@ -10,10 +10,4 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(selectedLevel);
     }
-
-    public void QuitGame()
-    {
-        Debug.Log("Quit!");
-        Application.Quit();
-    }
 }
