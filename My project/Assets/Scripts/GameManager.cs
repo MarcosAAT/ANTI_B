76290b189c9225ead7d614ab64b808bbcc79cff9
Npelletier91 <nicolas.pelletier@yahoo.com>
2024-05-17 16:07:47 -0400

    Created a game over menu with buttons to start over

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -1,29 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameManager : MonoBehaviour
 {
    // public Player player; 
     public TextMeshProUGUI scoreText;
-    private int score; 
+    public TextMeshProUGUI finalScoreText;
+    private int score;
+    public GameObject gameOverCanvas;
 
+    private void Start()
+    {
+        gameOverCanvas.SetActive(false);
+    }
     public void DecreaseScore(){
         score--; 
         scoreText.text =  score.ToString();
+        finalScoreText.text = score.ToString();
     }
 
     public void IncreseScore(){
         score++; 
         scoreText.text =  score.ToString();
+        finalScoreText.text = score.ToString();
+
     }
 
     public void Pause(){
-        Time.timeScale= 0f; 
+        Time.timeScale= 0f;
         //player.enabled= false; 
+
     }
     public void GameOver(){
-        Pause(); 
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0f;
+
     }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1f;
+
+    }
+
+    public void ReturnMainMenu()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
+
+    }
+
 }
--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -24,6 +24,8 @@ public class Player: MonoBehaviour{
     public float gravity = -9.81f;
     public float tilt = 5f;
 
+    public bool isGameOver = false;
+
     private SpriteRenderer spriteRenderer;
     private int spriteIndex;
     public Sprite[] sprites;
@@ -37,22 +39,28 @@ public class Player: MonoBehaviour{
     private void Start()
     {
         InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+        isGameOver = false;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
-            direction = Vector3.up * strength;
+        // This will stop the character from moving after the game is over
+        if(isGameOver == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                direction = Vector3.up * strength;
+            }
+
+            // Apply gravity and update the position
+            direction.y += gravity * Time.deltaTime;
+            transform.position += direction * Time.deltaTime;
+
+            // Tilt the bird based on the direction
+            Vector3 rotation = transform.eulerAngles;
+            rotation.z = direction.y * tilt;
+            transform.eulerAngles = rotation;
         }
-
-        // Apply gravity and update the position
-        direction.y += gravity * Time.deltaTime;
-        transform.position += direction * Time.deltaTime;
-
-        // Tilt the bird based on the direction
-        Vector3 rotation = transform.eulerAngles;
-        rotation.z = direction.y * tilt;
-        transform.eulerAngles = rotation;
     }
 
     private void AnimateSprite()
@@ -75,8 +83,10 @@ public class Player: MonoBehaviour{
 
         } else if(other.gameObject.tag == "ScoreWine"){
             FindObjectOfType<GameManager>().DecreaseScore();
+
         } else if(other.gameObject.tag == "Ground"){
-             FindObjectOfType<GameManager>().Pause();
+            FindObjectOfType<GameManager>().GameOver();
+            isGameOver = true;
         }
     }
 
