a224b51b2eb24a546a9b886ae10c191dc9059b8f
Npelletier91 <nicolas.pelletier@yahoo.com>
2024-05-16 21:12:18 -0400

    Added multiple pixel art buttons for the main menu

--- My project/Assets/Scripts/MainMenu.cs
+++ My project/Assets/Scripts/MainMenu.cs
@@ -2,10 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public int selectedLevel = 1;
+
+    public Image birdImage;
+    private int spriteIndex;
+    public Sprite[] sprites;
+
+    public float moveDistance = 1.0f; 
+    public float moveSpeed = 1.0f;
+
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateBirdSprite), 0.5f, 0.5f);
+
+        startPosition = birdImage.transform.position;
+    }
+
+    void Update()
+    {
+        // Bird Annimation up and down
+        Vector3 newPosition = startPosition + Vector3.up * Mathf.Sin(Time.time * moveSpeed) * moveDistance;
+        birdImage.transform.position = newPosition;
+    }
+
+    private void AnimateBirdSprite()
+    {
+        Debug.Log("calling annimation");
+        spriteIndex++;
+
+        if (spriteIndex >= sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+
+
+        birdImage.sprite = sprites[spriteIndex];
+
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(selectedLevel);
