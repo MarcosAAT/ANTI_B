542862de0419c0e5b1e6e8f4121b0b421c07bdf6
bowershaley <156343634+bowershaley@users.noreply.github.com>
2024-07-18 12:11:12 +0100

    Updated Mixers to work in all scenes
    background music plays constantly. Each drink + ANTI has a sound effect. Badger has flap sounds. Badger has death sounds.

--- My project/Assets/Scripts/SoundMixerManager.cs
+++ My project/Assets/Scripts/SoundMixerManager.cs
@@ -2,24 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundMixerManager : MonoBehaviour
+
+
 {
 [SerializeField] private AudioMixer audioMixer;
+[SerializeField] private Slider musicSlider;
+[SerializeField] private Slider sfxSlider;
 
-public void SetMasterVolume(float level)
+private void Start()
 {
-audioMixer.SetFloat("masterVolume", level);
+    if (PlayerPrefs.HasKey("mxVolume"))
+    {
+        LoadVolume();
+    }
+    else
+    {
+        SetMusicVolume();
+        SetSoundFXVolume();
+    }
+   
 }
 
-public void SetSoundFXVolume(float level)
+
+
+public void SetSoundFXVolume()
 {
-audioMixer.SetFloat("sfxVolume", level);
+float volume = sfxSlider.value;
+audioMixer.SetFloat("sfxVolume", Mathf.Log10(volume)*20);
+PlayerPrefs.SetFloat("soundVolume", volume);
 }
 
-public void SetMusicVolume(float level)
+public void SetMusicVolume()
 {
-audioMixer.SetFloat("musicVolume", level);
+float volume = musicSlider.value;
+audioMixer.SetFloat("musicVolume", Mathf.Log10(volume)*20);
+PlayerPrefs.SetFloat("mxVolume", volume);
+}
+
+private void LoadVolume()
+{
+    musicSlider.value = PlayerPrefs.GetFloat("mxVolume");
+    sfxSlider.value = PlayerPrefs.GetFloat("soundVolume");
+
+   
+
+    SetMusicVolume();
+    SetSoundFXVolume();
 }
 
 }
+
